18cc1ba59616786076111a393c9966ccc1215d30
Jagan <jagannathk79@gmail.com>
2018-09-09 11:47:29 -0700

    TextBin updates
    Endpoints moved as sub-app,  added signup and removed the separate admin app

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 var express = require('express');
-var firebase = require('firebase');
+var firebaseTB = require('firebase');
+
+var server_port = process.env.PORT || 5000;
+var signup_key = process.env.SIGNUPKEY;
 
 var app = express();
 
-var config = {
-    apiKey: "AIzaSyBLQPgC2j9okz3zU9ms2oe0foW6kXMvDj8",
-    authDomain: "textbin-9ca75.firebaseapp.com",
-    databaseURL: "https://textbin-9ca75.firebaseio.com",
-    storageBucket: "textbin-9ca75.appspot.com"
-};
-firebase.initializeApp(config);
-
-var server_port = process.env.PORT || 5000;
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-// Get a reference to the database service
-var database = firebase.app().database();
-
-// To allow CORS
+// ---- To allow CORS -----
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -26,9 +15,22 @@ app.use(function(req, res, next) {
 app.use(express.urlencoded());  // for parsing application/x-www-form-urlencoded
 app.use(express.json());        // for parsing application/json
 
-app.get('/:ownkey', function(req, res) {
+// ---- TextBin Instance ------
+var configTB = {
+    apiKey: "AIzaSyBLQPgC2j9okz3zU9ms2oe0foW6kXMvDj8",
+    authDomain: "textbin-9ca75.firebaseapp.com",
+    databaseURL: "https://textbin-9ca75.firebaseio.com",
+    storageBucket: "textbin-9ca75.appspot.com"
+};
+firebaseTB.initializeApp(configTB);
+var databaseTB = firebaseTB.app().database();
+
+/*******************************
+ *     TextBin Endpoints
+ ******************************/
+app.get('/textbin/:ownkey', function(req, res) {
     var ownkey = req.params.ownkey;
-    var rootRef = database.ref();
+    var rootRef = databaseTB.ref();
     var ownerRef = rootRef.child('Owners');
     var clipsRef = rootRef.child('Clips');
 
@@ -58,10 +60,9 @@ app.get('/:ownkey', function(req, res) {
         }
     });
 });
-
-app.post('/:ownkey', function(req, res) {
+app.post('/textbin/:ownkey', function(req, res) {
     var ownkey = req.params.ownkey;
-    var rootRef = database.ref();
+    var rootRef = databaseTB.ref();
     var ownerRef = rootRef.child('Owners');
     var clipsRef = rootRef.child('Clips');
 
@@ -80,7 +81,7 @@ app.post('/:ownkey', function(req, res) {
                 if(data.enabled) {
                     var dataRef = clipsRef.child(ownkey);
                     dataRef.push(newData);
-                    res.redirect('/'+ownkey);
+                    res.redirect('/textbin/'+ownkey);
                 } else {
                     res.status(200).send('callbackClips({"error":"Not Enabled"});');
                 }
@@ -93,14 +94,49 @@ app.post('/:ownkey', function(req, res) {
         var dataOwnRef = clipsRef.child(ownkey);
         var dataRef = dataOwnRef.child(clipid);
         dataRef.remove();
-        res.redirect('/'+ownkey);
+        res.redirect('/textbin/'+ownkey);
     }
 });
 
-// app.listen(server_port, server_ip_address, function () {
-//     console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
-// });
 
+/*******************************
+ *     Global Endpoints
+ ******************************/
+app.get('/signup', function(req, res) {
+    if(req.param('user') && req.param('app') && req.param('skey')) {
+    	if(req.param('app') == "textbin" && req.param('skey') == signup_key) {
+			var rootRef = databaseTB.ref();
+			var ownerRef = rootRef.child('Owners');
+			var clipRef = rootRef.child('Clips');
+			var usersRef = ownerRef.push({
+				username: req.param('user').charAt(0).toUpperCase() + req.param('user').substr(1),
+				email: req.param('user')+"@email.com",
+				enabled: true,
+				quota: 20
+			});
+			var encClip = new Buffer("Welcome to your TextBin!", "utf8").toString('base64');
+			var newData = {
+				"type" : "text",
+				"data": encClip,
+				"state": "N",
+				"modified": (new Date()).getTime() * -1
+			};
+			clipRef.child(usersRef.key).push(newData);
+			res.status(200).send('New Account Created for: ' + req.param('user').charAt(0).toUpperCase() + req.param('user').substr(1) + '\nAccount Key: ' + usersRef.key);
+		} else {
+			res.status(200).send('Invalid app or key');
+		}
+    } else {
+    	res.status(200).send('Missing required data');
+    }
+});
+app.get('/', function(req, res) {
+   res.status(200).send('<br/><center><h2>Welcome to JK\'s Node module Home</h2><p>Well, there\'s nothing to view here...</p></center>');
+});
+
+/*******************************
+ *     Startup App
+ ******************************/
 app.listen(server_port, function () {
     console.log( "Listening to " + server_port  );
 });
